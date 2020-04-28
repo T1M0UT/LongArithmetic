004c1c874e4f1870b1b62646a9cb23d5cc481b3c
T1M0UT <tymur.krasnianskiy@gmail.com>
2020-04-28 14:17:47 +0300

    fix+new features

--- LongArithmetic/LongNumber.cs
+++ LongArithmetic/LongNumber.cs
@@ -6,20 +6,40 @@ using System.Threading.Tasks;
 
 namespace LongArithmetic
 {
-    class LongNumber
+    public class LongNumber
     {
         private List<int> digits;
-
         public int Length { get => digits.Count; }
-
-        public LongNumber(List<int> digits)
+        public bool IsPositive { get; private set; }
+        public LongNumber(List<int> input, bool IsPositive)
         {
-            digits.Reverse();
-            this.digits = digits;
+            digits = new List<int>(input);
+            this.IsPositive = IsPositive;
         }
-
+        public LongNumber(in string number)
+        {
+            char[] arr = number.ToCharArray();
+            digits = new List<int>();
+            if (arr.Length >= 1 && arr[0] == '-') IsPositive = false;
+            else
+            {
+                IsPositive = true;
+                digits.Add(arr[0]-'0');
+            }
+            for (int i=arr.Length-1; i>0; i--)
+            {
+                
+                digits.Add(arr[i]-'0');
+            }
+        }
         public LongNumber(int number)
         {
+            if (number < 0)
+            {
+                IsPositive = false;
+                number *= -1;
+            }
+            else IsPositive = true;
             digits = new List<int>();
             if (number == 0)
             {
@@ -33,26 +53,13 @@ namespace LongArithmetic
                 number /= 10;
             }
         }
-        public static implicit operator String(LongNumber number)
-        {
-            return string.Join("",number.digits);
-        }
-        public static implicit operator LongNumber(int number)
-        {
-            return new LongNumber(number);
-        }
-        /*public static LongNumber operator >(LongNumber a, LongNumber b)
-        {
-            return a - b > 0;
-        }*/
-        public static LongNumber operator +(LongNumber a, LongNumber b)
+        
+        private static LongNumber Plus(LongNumber a, LongNumber b, bool znakRes)
         {
             LongNumber bigger = a.Length > b.Length ? a : b;
             LongNumber smaller = a.Length <= b.Length ? a : b;
-
             List<int> result = new List<int>();
             int brain = 0;
-
             for (int i = 0; i < bigger.Length; i++)
             {
                 int sum;
@@ -60,41 +67,231 @@ namespace LongArithmetic
                 else sum = bigger.digits[i] + brain;
                 brain = sum / 10;
                 int newDigit = sum % 10;
-
                 result.Add(newDigit);
             }
 
             if (brain != 0)
                 result.Add(brain);
 
-            return new LongNumber(result);
-        }
-        public static LongNumber operator *(LongNumber a, LongNumber b)
-        {
-            LongNumber bigger = a.Length > b.Length ? a : b;
-            LongNumber smaller = a.Length <= b.Length ? a : b;
+            return new LongNumber(result, znakRes);
 
+        }
+        private static LongNumber Minus(in LongNumber a, in LongNumber b, bool znakRes)
+        {
+            LongNumber bigger;
+            LongNumber smaller;
+            if (a.Length != b.Length)
+            {
+                bigger = a.Length > b.Length ? a : b;
+                smaller = a.Length < b.Length ? a : b;
+            }
+            else
+            {
+                bigger = Bigger(a, b) ? a : b;
+                smaller = Bigger(a, b) ? b : a;
+            }
             List<int> result = new List<int>();
             int brain = 0;
-
             for (int i = 0; i < bigger.Length; i++)
             {
-                for (int j = 0; j < smaller.Length; j++)
+                int divide;
+                if (i < smaller.Length)
                 {
-                    int current;
-                    if (i < smaller.Length) current = (bigger.digits[i] * smaller.digits[j]) + brain;
-                    else current = bigger.digits[i] + brain;
-                    brain = current / 10;
-                    int newDigit = current % 10;
-
-                    result.Add(newDigit);
+                    if (bigger.digits[i] >= smaller.digits[i])
+                        divide = bigger.digits[i] - smaller.digits[i];
+                    else
+                    {
+                        bigger.digits[i + 1] -= 1;
+                        divide = bigger.digits[i] + 10 - smaller.digits[i];
+                    }
                 }
+                else divide = bigger.digits[i];
+                brain = divide / 10;
+                int newDigit = divide % 10 + brain;
+
+                result.Add(newDigit);
             }
 
             if (brain != 0)
                 result.Add(brain);
-
-            return new LongNumber(result);
+            return NaxuiNuli(new LongNumber(result, znakRes));
         }
+        private static LongNumber NaxuiNuli(LongNumber a)
+        {
+            for (int i = a.Length - 1; i > 0 ; i--)
+            {
+                if (a.digits[i] == 0) a.digits.RemoveAt(i);
+                else return a;
+            }
+            if (a.Length==1 && a.digits[0]==0)
+            {
+                a.IsPositive = true;
+                a = 0;
+            }
+            return a;
+        }
+        public override string ToString()
+        {
+            List<int> output = new List<int>(digits);
+            output.Reverse();
+            return (IsPositive ? "" : "-") + string.Join("", output);
+        }
+        public static implicit operator LongNumber(int number)
+        {
+            return new LongNumber(number);
+        }
+
+        public static LongNumber operator +(LongNumber a, LongNumber b)
+        {
+            bool znak = Bigger(a, b);
+            if (!a.IsPositive && b.IsPositive) return Minus(b, a, znak);
+            if (a.IsPositive && !b.IsPositive) return Minus(a, b, znak);
+            return Plus(a, b, znak);
+        }
+        public static LongNumber operator -(LongNumber a, LongNumber b)
+        {
+            bool znak = Bigger(a, b);
+            if (a.IsPositive && !b.IsPositive) return Plus(a, b, znak);
+            if (!a.IsPositive && b.IsPositive) return Plus(b, a, znak);
+            return Minus(a, b, znak);
+        }
+        private static bool Bigger(in LongNumber a, in LongNumber b)
+        {
+            if (a.IsPositive && !b.IsPositive) return true;
+            if (!a.IsPositive && b.IsPositive) return false;
+            if (a.Length > b.Length && a.IsPositive) return true;
+            if (a.Length > b.Length && !a.IsPositive) return false;
+            if (a.Length < b.Length && b.IsPositive) return false;
+            if (a.Length < b.Length && !b.IsPositive) return true;
+            if (a.IsPositive)
+            {
+                for (int i = a.Length - 1; i >= 0; i--)
+                {
+                    if (a.digits[i] > b.digits[i]) return true;
+                    if (a.digits[i] < b.digits[i]) return false;
+                }
+                return false; //Equals
+            }
+            else
+            {
+                for (int i = a.Length - 1; i >= 0; i--)
+                {
+                    if (a.digits[i] > b.digits[i]) return false;
+                    if (a.digits[i] < b.digits[i]) return true;
+                }
+                return false;    //Equals
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is LongNumber number &&
+                   EqualityComparer<List<int>>.Default.Equals(digits, number.digits) &&
+                   Length == number.Length &&
+                   IsPositive == number.IsPositive;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1224394762;
+            hashCode = hashCode * -1521134295 + EqualityComparer<List<int>>.Default.GetHashCode(digits);
+            hashCode = hashCode * -1521134295 + Length.GetHashCode();
+            hashCode = hashCode * -1521134295 + IsPositive.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator >(LongNumber a, LongNumber b)
+        {
+            return Bigger(a, b);
+        }
+        public static bool operator <(LongNumber a, LongNumber b)
+        {
+            return Bigger(b, a);
+        }
+        public static bool operator >=(LongNumber a, LongNumber b)
+        {
+            if (Bigger(a, b)) return true;
+            return !Bigger(b, a);
+        }
+        public static bool operator <=(LongNumber a, LongNumber b)
+        {
+            if (!Bigger(a, b)) return true;
+            return false;
+        }
+
+        public static bool operator !=(LongNumber a, LongNumber b)
+        {
+            return !(a == b);
+        }
+        public static bool operator ==(LongNumber a, LongNumber b)
+        {
+            return !Bigger(a, b) && !Bigger(b, a);
+        }
+        public static LongNumber operator ++(LongNumber a)
+        {
+            return a += 1;
+        }
+        public static LongNumber operator *(LongNumber a, int b)
+        {
+            bool znakRes = b < 0 ? false : true;
+            if (b < 0) b *= -1;
+            if (!a.IsPositive) znakRes = !znakRes;
+            LongNumber zeroHelper = 1;
+            LongNumber brain = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                brain += (a.digits[i] * b) * zeroHelper;
+                zeroHelper *= 10;
+            }  
+            LongNumber c = new LongNumber(brain.digits,znakRes);
+            return c;
+        }
+        public static LongNumber operator *(LongNumber a, LongNumber b)
+        {
+            bool znakRes = a.IsPositive? true : false;
+            if (!b.IsPositive) znakRes = !znakRes;
+            LongNumber zeroHelper = 1;
+            LongNumber brain = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                LongNumber innerZeroHelper = zeroHelper;
+                for (int j = 0; j < b.Length; j++)
+                {
+                    brain += (a.digits[i] * b.digits[j]) * innerZeroHelper;
+                    innerZeroHelper *= 10;
+                }
+                zeroHelper *= 10;
+            }
+            LongNumber c = new LongNumber(brain.digits,znakRes);
+            return c;
+        }
+        public static LongNumber operator /(LongNumber a, LongNumber b)
+        {
+            bool znakRes = a.IsPositive ? true : false;
+            if (!b.IsPositive) znakRes = !znakRes;
+            LongNumber result=0;
+            LongNumber e = new LongNumber(a.digits,znakRes); 
+            while (e>=b)
+            {
+                e -= b;
+                result++;
+            }
+            return result;
+        }
+        public static LongNumber operator %(LongNumber a, LongNumber b)
+        {
+            bool znakRes = a.IsPositive ? true : false;
+            if (!b.IsPositive) znakRes = !znakRes;
+            LongNumber result = 0;
+            LongNumber e = new LongNumber(a.digits, znakRes);
+            while (e >= b)
+            {
+                e -= b;
+                result++;
+            }
+            return e;
+        }
+
+
     }
 }
--- LongArithmetic/Program.cs
+++ LongArithmetic/Program.cs
@@ -7,10 +7,28 @@ namespace LongArithmetic
     {
         public static void Main()
         {
-            List<int> q = new List<int>() { 1 };
-            LongNumber a = new LongNumber(new List<int>() { 1,2 });
-            int b = 2;
-            Console.WriteLine(a*b);
+            LongNumber a = new LongNumber(new List<int>() { 0, 0, 1 }, true);
+            LongNumber c = new LongNumber(new List<int>() { 0, 2 }, true);
+            // int b = 200;
+            // LongNumber d = new LongNumber(new List<int>() { 0, 1 }, true);
+            // d += b;
+            LongNumber q = new LongNumber("-123");
+            Console.WriteLine(Factorial(10));
+        }
+
+        public static LongNumber Factorial(int inputValue)
+        {
+            LongNumber[] fact = new LongNumber[inputValue + 1];
+            fact[0] = new LongNumber("0");
+            fact[1] = new LongNumber("1");
+            fact[2] = new LongNumber("2");
+            LongNumber j = new LongNumber("3");
+            for (int i = 3; i <= inputValue - 2; i++)
+            {
+
+                fact[i] = fact[i - 1] * j;
+            }
+            return fact[inputValue - 2];
         }
     }
 
